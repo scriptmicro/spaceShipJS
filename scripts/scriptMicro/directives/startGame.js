276015fbe5d0c462125f04a5137a01339f66a1c2
Clinton White <flashranger@gmail.com>
2017-02-16 23:55:26 -0800

    On Refresh
    The start and end buttons would get stuck at the top left after a reload
    or refresh.

--- scripts/scriptMicro/directives/startGame.js
+++ scripts/scriptMicro/directives/startGame.js
@@ -9,6 +9,7 @@ app.directive('startGame', function () {
 
 			console.log("startGame");
 			var startBTN = document.getElementById("startBTN");
+            var container = document.getElementById("container");
 
 			TweenMax.set(startBTN, { x: (container.clientWidth / 2) - (startBTN.clientWidth / 2), y: 0-startBTN.clientHeight});
     		var startButtonTween = new TweenMax.to(startBTN, 0.5, { x: (container.clientWidth / 2) - (startBTN.clientWidth / 2), y: (container.clientHeight / 2) - (startBTN.clientHeight / 2), ease:  Back.easeOut});
@@ -16,10 +17,7 @@ app.directive('startGame', function () {
             element.bind('click', function () {
             	console.log("startGame click");
                 //TweenMax.set(element, { visibility: "hidden" });
-
 				TweenMax.to(startBTN, 0.5, { x: (container.clientWidth / 2) - (startBTN.clientWidth / 2), y: 0-startBTN.clientHeight, ease:  Back.easeOut});
-    			
-
             });
 
             element.bind('mouseenter', function () {
@@ -30,8 +28,6 @@ app.directive('startGame', function () {
             	console.log("startGame mouseleave");
                 TweenMax.to(startBTN, 0.5, { css: { backgroundColor: "#000000" }, ease: Power2.easeOut });
             });
-
-
         }
     }
 });
