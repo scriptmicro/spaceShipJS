6b5e95cb7e690a6f933a5ddc658f9ff3aa97d419
Clinton White <flashranger@gmail.com>
2017-03-01 23:22:47 -0800

    Changed over to typescript file ext
    -that was quick but we have a lot of data type errors to improve
    -added typescript to the gulpfile.js and create the javascript
    -I see alot of room for improvement!

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,28 @@ var image = require('gulp-image');
 var gulpCopy = require('gulp-copy');
 var htmlreplace = require('gulp-html-replace');
 
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task('clean', function() {
-	console.log("Clean all files in build folder");
-	return gulp.src("./build/*", { read: false }).pipe(clean());
+gulp.task('cleanAll', function () {
+    console.log("Clean all files in build folder");
+    return gulp.src(["./build/*"], { read: false }).pipe(clean());
 });
 
 
-gulp.task('compress', ['clean'], function(){
-    return gulp.src(['./scripts/scriptMicro/scriptMicro.js', 
-                    './scripts/scriptMicro/shapeBuilder.js', 
-                    './scripts/scriptMicro/utilities.js',
-                    './scripts/scriptMicro/directives/*.js',
-                    './scripts/scriptMicro/controllers/*.js'])
+gulp.task('typescript', ['cleanAll'], function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("./scripts/scriptMicro/"));
+});
+
+
+gulp.task('compress', ['typescript'], function () {
+    return gulp.src(['./scripts/scriptMicro/scriptMicro.js',
+        './scripts/scriptMicro/shapeBuilder.js',
+        './scripts/scriptMicro/utilities.js',
+        './scripts/scriptMicro/directives/*.js',
+        './scripts/scriptMicro/controllers/*.js'])
         .pipe(concat('concat.js'))
         //.pipe(gulp.dest('./build/scripts/scriptMicro/'))
         .pipe(rename('scriptMicro.min.js'))
@@ -29,17 +38,33 @@ gulp.task('compress', ['clean'], function(){
         .pipe(gulp.dest('./build/scripts/scriptMicro/'));
 });
 
-gulp.task('movejs', ['compress'], function(){
-    return gulp.src(['./scripts/angularJS/**/*', './scripts/greenSock/**/*'])
-        .pipe(gulpCopy('./build/scripts/', { prefix: 1 }))
 
+gulp.task('cleanJS', ['compress'], function () {
+    console.log("Clean all JS files in scripts (dev) folder");
+    
+    //This is a nice to have :
+    //A) in one case the js files are gone and the directory is easy to look at and understand
+    //B) js files are visible/available and able to be debugged without concat and uglify
+    
+
+    return gulp.src(['./scripts/scriptMicro/scriptMicro.js', 
+                   './scripts/scriptMicro/shapeBuilder.js', 
+                   './scripts/scriptMicro/utilities.js',
+                   './scripts/scriptMicro/directives/*.js',
+                   './scripts/scriptMicro/controllers/*.js'], { read: false })
+           .pipe(clean());
+             
 });
 
 
+gulp.task('movejs', ['cleanJS'], function () {
+    return gulp.src(['./scripts/angularJS/**/*', './scripts/greenSock/**/*'])
+        .pipe(gulpCopy('./build/scripts/', { prefix: 1 }))
+});
 
-gulp.task('css', ['movejs'], function(){
+gulp.task('css', ['movejs'], function () {
     return gulp.src(['./css/*.css'])
-        .pipe(cleanCSS({debug: true}, function(details) {
+        .pipe(cleanCSS({ debug: true }, function (details) {
             console.log(details.name + ': ' + details.stats.originalSize);
             console.log(details.name + ': ' + details.stats.minifiedSize);
         }))
@@ -47,33 +72,28 @@ gulp.task('css', ['movejs'], function(){
         .pipe(gulp.dest('./build/css/'));
 });
 
-
-gulp.task('html', ['css'], function() {
-  return gulp.src('./templates/*.html')
-    .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
-    .pipe(gulp.dest('./build/templates'));
+gulp.task('html', ['css'], function () {
+    return gulp.src('./templates/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
+        .pipe(gulp.dest('./build/templates'));
 });
 
-
-gulp.task('index', ['html'], function() {
-  return gulp.src('index.html')
-    .pipe(htmlreplace({
-        'css': 'css/index.min.css',
-        'js': 'scripts/scriptMicro/scriptMicro.min.js'
-        //'script': 'scripts/scriptMicro/scriptMicro.min.js'
-    }))
-    .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
-    .pipe(gulp.dest('./build/'));
+gulp.task('index', ['html'], function () {
+    return gulp.src('index.html')
+        .pipe(htmlreplace({
+            'css': 'css/index.min.css',
+            'js': 'scripts/scriptMicro/scriptMicro.min.js'
+        }))
+        .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
+        .pipe(gulp.dest('./build/'));
 });
 
-
-gulp.task('images', ['index'], function() {
-  return gulp.src('./images/*')
-    .pipe(image())
-    .pipe(gulp.dest('./build/images/'));
+gulp.task('images', ['index'], function () {
+    return gulp.src('./images/*')
+        .pipe(image())
+        .pipe(gulp.dest('./build/images/'));
 });
 
-
-gulp.task('default', ['clean','compress', 'movejs', 'css', 'html', 'index', 'images'], function() {
+gulp.task('default', ['cleanAll', 'typescript', 'compress', 'cleanJS', 'movejs', 'css', 'html', 'index', 'images'], function () {
 
 });
